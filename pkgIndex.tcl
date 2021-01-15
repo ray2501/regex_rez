56195b309ae9895ee43c55e249cd8328f1510eab
Danilo Chang <ray2501@gmail.com>
2021-01-15 21:27:26 +0800

    Bump version to v0.3

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,5 @@
 #
 # Tcl package index file
 #
-package ifneeded regex_rez 0.2 \
-    [list load [file join $dir libregex_rez0.2.so] regex_rez]
+package ifneeded regex_rez 0.3 \
+    [list load [file join $dir libregex_rez0.3.so] regex_rez]
