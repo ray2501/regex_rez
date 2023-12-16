81564d143806e535dd399bd31ff15c10c039fd26
Danilo Chang <ray2501@gmail.com>
2023-12-16 18:42:24 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded regex_rez 0.3 \
-    [list load [file join $dir libregex_rez0.3.so] regex_rez]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded regex_rez 0.3 \
+	    [list load [file join $dir libtcl9regex_rez0.3.so] [string totitle regex_rez]]
+} else {
+    package ifneeded regex_rez 0.3 \
+	    [list load [file join $dir libregex_rez0.3.so] [string totitle regex_rez]]
+}
